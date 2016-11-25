7fed5e8154b16bd84099a095a05e4f2bc3f1e51f
ysoftman <ysoftman@gmail.com>
2016-11-25 13:50:56 +0900

    usage 설명 추가. decbase64 함수 추가.

--- enchash.go
+++ enchash.go
@@ -9,37 +9,41 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
-	"log"
 	"net/url"
 	"os"
+	"path/filepath"
 )
 
+func showUsage(file string) {
+	fmt.Println("[usage] " + file + " string")
+}
 func main() {
-	fmt.Println("~ enchash ~")
+	_, file := filepath.Split(os.Args[0])
 	if len(os.Args) <= 1 {
-		fmt.Printf("ex)%s string\n", os.Args[0])
+		showUsage(file)
 		return
 	}
-	log.Printf("target = %s", os.Args[1])
+	fmt.Println("string =", os.Args[1])
 	GetMD5(os.Args[1])
 	GetSha1(os.Args[1])
 	GetSha256(os.Args[1])
-	GetURLEnc(os.Args[1])
-	GetBase64(os.Args[1])
+	GetEncURL(os.Args[1])
+	GetEncBase64(os.Args[1])
+	GetDecBase64(os.Args[1])
 }
 
 // GetMD5 : md5 생성하기
 func GetMD5(str string) string {
 	md5 := md5.New()
 	data := []byte(str)
-	// log.Printf("%s\n", data)
+	// fmt.Printf("%s\n", data)
 
 	// 해시값을 만들 데이터 설정
 	md5.Write(data)
-	// log.Printf("md5 sum = %x\n", md5.Sum(nil))
+	// fmt.Printf("md5 sum = %x\n", md5.Sum(nil))
 	// md5str := (string)(md5.Sum(nil))
 	md5str := hex.EncodeToString(md5.Sum(nil))
-	log.Printf("md5 = %s\n", md5str)
+	fmt.Printf("md5 = %s\n", md5str)
 
 	return md5str
 }
@@ -49,14 +53,14 @@ func GetSha1(str string) string {
 	// sha1 해시값 만들기
 	sha1 := sha1.New()
 	data := []byte(str)
-	// log.Printf("%s\n", data)
+	// fmt.Printf("%s\n", data)
 	// 해시값을 만들 데이터 설정
 	sha1.Write(data)
 	// 해시값을 출력
 	// 추가 data 를 넣어주면 기존 데이터에 sum 하는 방식으로 해시값을 리턴
 	// data 1개인 경우 sum(nil) 사용
 	sha1str := hex.EncodeToString(sha1.Sum(nil))
-	log.Printf("sha1 = %s\n", sha1str)
+	fmt.Printf("sha1 = %s\n", sha1str)
 	return sha1str
 }
 
@@ -65,28 +69,35 @@ func GetSha256(str string) string {
 	// sha256 해시값 만들기
 	sha256 := sha256.New()
 	data := []byte(str)
-	// log.Printf("%s\n", data)
+	// fmt.Printf("%s\n", data)
 	// 해시값을 만들 데이터 설정
 	sha256.Write(data)
 	// 해시값을 출력
 	// 추가 data 를 넣어주면 기존 데이터에 sum 하는 방식으로 해시값을 리턴
 	// data 1개인 경우 sum(nil) 사용
 	sha256str := hex.EncodeToString(sha256.Sum(nil))
-	log.Printf("sha256 = %s\n", sha256str)
+	fmt.Printf("sha256 = %s\n", sha256str)
 	return sha256str
 }
 
-// GetURLEnc : URL 인코딩 생성하기
-func GetURLEnc(str string) string {
-	urlenc := url.QueryEscape(str)
-	log.Printf("urlenc = %s\n", urlenc)
-	return urlenc
+// GetEncURL : URL 인코딩
+func GetEncURL(str string) string {
+	encurl := url.QueryEscape(str)
+	fmt.Printf("encode url = %s\n", encurl)
+	return encurl
 }
 
-// GetBase64 :  base64 생성하기
-func GetBase64(str string) string {
+// GetEncBase64 :  base64 인코딩
+func GetEncBase64(str string) string {
 	data := []byte(str)
-	base64 := base64.StdEncoding.EncodeToString(data)
-	log.Printf("base64 = %s\n", base64)
-	return base64
+	encbase64 := base64.StdEncoding.EncodeToString(data)
+	fmt.Printf("encode base64 = %s\n", encbase64)
+	return encbase64
+}
+
+// GetDecBase64 :  base64 디코딩
+func GetDecBase64(str string) string {
+	decbase64, _ := base64.StdEncoding.DecodeString(str)
+	fmt.Printf("decode base64 = %s\n", decbase64)
+	return string(decbase64)
 }
